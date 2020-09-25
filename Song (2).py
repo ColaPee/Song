682f96e54099f1cb0234da6b1981340b1d4e57cb
ColaPee <71814923+ColaPee@users.noreply.github.com>
2020-09-25 09:59:52 +0800

    Add files via upload

---
+++ Song (2).py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Song  = ('Never Gonna Give You Up')
+print (Song)
+
+
+# In[3]:
+
+
+YearReleased = (1987)
+print (YearReleased)
+
+
+# In[4]:
+
+
+Artist = ('Rick Astley')
+print (Artist)
+
+
+# In[5]:
+
+
+Album = ('Whenever You Need Somebody')
+print (Album)
+
+
+# In[7]:
+
+
+Genre = ('pop')
+print (Genre)
+
+
+# In[10]:
+
+
+views = (768,767,400)
+
+
+# In[11]:
+
+
+print (views)
+
+
+# In[12]:
+
+
+LenghtInSeconds = (213)
+print (LenghtInSeconds)
+
